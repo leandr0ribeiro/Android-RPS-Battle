c3a973fb5f1e315ff0c32167ee3447b33a8223bc
Leandro Ribeiro de Souza <leandrosou@abcbrasil.com.br>
2023-12-20 11:23:49 -0300

    primeiro commit

---
+++ app/src/main/java/com/mrxleet/jokenpo/MainActivity.java
@@ -0,0 +1,76 @@
+package com.mrxleet.jokenpo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void selecionarPedra(View view){
+        verificarGanhador("pedra");
+    }
+
+    public void selecionarPapel(View view){
+        verificarGanhador("papel");
+    }
+
+    public void selecionarTesoura(View view){
+        verificarGanhador("tesoura");
+    }
+
+    private String gerarEscolhaAleatoriaApp() {
+        String opcoes [] = {"pedra","papel","tesoura"};
+        int numeroAleatorio = new Random().nextInt(3);
+
+        ImageView imagemApp = findViewById(R.id.image_app);
+        String escolhaApp = opcoes[numeroAleatorio];
+        switch (escolhaApp){
+            case "pedra":
+                imagemApp.setImageResource(R.drawable.pedra);
+                break;
+            case "papel":
+                imagemApp.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura":
+                imagemApp.setImageResource(R.drawable.tesoura);
+                break;
+        }
+        return escolhaApp;
+
+    }
+
+    private void verificarGanhador( String escolhaUsuario){
+        //System.out.println("Escolha do usuário " + escolhaUsuario);
+        String escolhaApp = gerarEscolhaAleatoriaApp();
+        TextView textResultado = findViewById(R.id.text_resultado);
+
+        if(
+                (escolhaApp == "pedra" && escolhaUsuario == "tesoura") ||
+                (escolhaApp == "papel" && escolhaUsuario == "pedra") ||
+                (escolhaApp == "tesoura" && escolhaUsuario == "papel")
+        ){
+            textResultado.setText("Você Perdeu!");
+        }else if(
+            (escolhaUsuario == "pedra" && escolhaApp == "tesoura") ||
+            (escolhaUsuario == "papel" && escolhaApp == "pedra") ||
+            (escolhaUsuario == "tesoura" && escolhaApp == "papel")
+        ){
+            textResultado.setText("Você Ganhou!");
+        } else {
+            textResultado.setText("Empatou!!");
+        }
+    }
+
+}
